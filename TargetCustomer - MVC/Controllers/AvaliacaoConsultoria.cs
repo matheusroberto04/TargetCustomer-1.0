0f20083ace5f0fd342568e4c2fad6545bcbbe792
matheusroberto04 <matheusmath152@gmail.com>
2024-05-16 14:20:50 -0300

    Finalizando CRUDS

---
+++ TargetCustomer - MVC/Controllers/AvaliacaoConsultoria.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TargetCustomer___MVC.Data;
+using TargetCustomer___MVC.DTOs;
+
+namespace TargetCustomer___MVC.Controllers
+{
+    public class AvaliacaoController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public AvaliacaoController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        // Exibir a página de Avaliações
+        public IActionResult AvaliacaoPage()
+        {
+            var avaliacoes = _dataContext.AvaliacaoConsultorias.ToList();
+            ViewBag.Avaliacoes = avaliacoes;
+            return View();
+        }
+
+        // Criar nova Avaliação
+        public IActionResult CriarAvaliacao(AvaliacaoDTO request)
+        {
+            AvaliacaoConsultoria novaAvaliacao = new AvaliacaoConsultoria
+            {
+                Id = request.Id,
+                TextoAvaliacao = request.TextoAvaliacao,
+            };
+
+            _dataContext.AvaliacaoConsultorias.Add(novaAvaliacao);
+            _dataContext.SaveChanges();
+
+            return RedirectToAction("AvaliacaoPage");
+        }
+
+        // Alterar Avaliação existente
+        public IActionResult AlterarAvaliacao(int id, AvaliacaoDTO request)
+        {
+            var avaliacao = _dataContext.AvaliacaoConsultorias.Find(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            avaliacao.Id = request.Id;
+            avaliacao.TextoAvaliacao = request.TextoAvaliacao;
+
+            _dataContext.Update(avaliacao);
+            _dataContext.SaveChanges();
+
+            return RedirectToAction("AvaliacaoPage");
+        }
+
+        // Excluir Avaliação
+        public IActionResult ExcluirAvaliacao(int id)
+        {
+            var avaliacao = _dataContext.AvaliacaoConsultorias.Find(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.AvaliacaoConsultorias.Remove(avaliacao);
+            _dataContext.SaveChanges();
+
+            return RedirectToAction("AvaliacaoPage");
+        }
+    }
+
+}
---
+++ TargetCustomer - MVC/DTOs/AvaliacaoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TargetCustomer___MVC.DTOs
+{
+    public class AvaliacaoDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Usuario { get; set;}
+        [Required]
+        public string TextoAvaliacao { get; set;}
+    }
+}
